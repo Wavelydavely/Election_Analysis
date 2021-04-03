1537e01184f74bfa54f3fb00d83e2e430d0d7e40
David Coyle <76623937+Wavelydavely@users.noreply.github.com>
2021-04-03 18:30:24 -0500

    9th time's a charm

--- PyPoll.py
+++ PyPoll.py
@@ -21,8 +21,6 @@ with open(file_to_load) as election_data:
     headers = next(file_reader)
     print(headers)
 
-    
-
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
     # Write some data to the file.
