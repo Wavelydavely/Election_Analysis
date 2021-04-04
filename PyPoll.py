e4051cd0a56f96954e4d83a80c82bf2c6abfb48b
David Coyle <76623937+Wavelydavely@users.noreply.github.com>
2021-04-03 20:35:26 -0500

    :.2f

--- PyPoll.py
+++ PyPoll.py
@@ -1,4 +1,3 @@
-# The data we need to retrieve.
 # Add our dependencies
 import csv
 import os
@@ -7,24 +6,42 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Counter
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:  
-    #headers = (next(csvreader))
 
-    # To do: read and analyze the data here)
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
-    # Print each row in the CSV file.
-    #for row in file_reader:
-        #print(row)
-    # Print header row
+
+    # Read the header row
     headers = next(file_reader)
-    print(headers)
+
+    # Add to the total vote count and candidate list
+    for row in file_reader:       
+        total_votes += 1
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1
+# Determine percentage of votes for each candidate
+# 1. Iterate through candidate list
+for candidate_name in candidate_votes:
+    # 2. Retreive vote count of a candidate
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate percentage of votes
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name & percentage of votes
+    print(f"{candidate_name}: received {vote_percentage:.2f}% of the vote.")
 
 # Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+#with open(file_to_save, "w") as txt_file:
     # Write some data to the file.
-    txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+    #txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
 
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
